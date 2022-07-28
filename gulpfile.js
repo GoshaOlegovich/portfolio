0a55f8d16809272c191bc7e2333521a901df44df
_webninja <web.ingvarr@gmail.com>
2022-07-28 19:02:22 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+
+const gulp = require('gulp');
+
+//Css
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+//Js
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+
+//Other
+
+const fileinclude = require('gulp-file-include');
+const browserSync = require('browser-sync').create();
+
+
+
+
+
+var plugins = [
+    autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+        cascade: false
+    }),
+    cssnano()
+];
+
+
+
+function minJs() {
+    return gulp.src(['src/script/*.js'])
+    //.pipe(uglify())  - not use now
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('./build/script'))
+    .pipe(browserSync.stream());
+}
+
+
+function include() {
+    return gulp.src(['src/html/index.html'])
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('./build'))
+        .pipe(browserSync.stream());
+}
+
+
+
+
+function style() {
+    return gulp.src('src/scss/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('./build/styles'))
+        .pipe(browserSync.stream());
+}
+
+
+
+
+
+
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+    gulp.watch("src/scss/**/*.scss", style)
+    gulp.watch("src/script/**/*.js", minJs)
+    gulp.watch("src/html/**/*.html", include).on('change', browserSync.reload)
+
+}
+
+
+
+
+function main() {
+    include()
+    style()
+    watch();
+    
+}
+
+
+exports.default = main
