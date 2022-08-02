4e90e2a425e5943209e25b4035be53a1fb58b1e7
_webninja <web.ingvarr@gmail.com>
2022-08-02 19:35:54 +0300

    fix mobile

--- src/script/script.js
+++ src/script/script.js
@@ -1,56 +1,56 @@
-const Header = document.querySelector('.header')
-const links = document.querySelectorAll('.header__link')
-const Services = document.querySelector('.services')
+// const Header = document.querySelector('.header')
+// const links = document.querySelectorAll('.header__link')
+// const Services = document.querySelector('.services')
 
-let servicesPos = Services.getBoundingClientRect().top
-
-
+// let servicesPos = Services.getBoundingClientRect().top
 
 
-window.addEventListener('scroll', () => {
-    let scrollY = window.scrollY
-    if (scrollY >= servicesPos) {
-        Header.style.top = `${servicesPos}px`;
-        setTimeout(() => {
-            Header.style.position = 'fixed';
-            Header.style.top = `0`;
-        }, 100);
+
+
+// window.addEventListener('scroll', () => {
+//     let scrollY = window.scrollY
+//     if (scrollY >= servicesPos) {
+//         Header.style.top = `${servicesPos}px`;
+//         setTimeout(() => {
+//             Header.style.position = 'fixed';
+//             Header.style.top = `0`;
+//         }, 100);
        
-        console.log('ok');
-    } else if (scrollY < servicesPos - 200){
-        Header.style.position = 'relative';
-    }
-})
+//         console.log('ok');
+//     } else if (scrollY < servicesPos - 200){
+//         Header.style.position = 'relative';
+//     }
+// })
 
 
 
-window.addEventListener('scroll', () => {
-    let scrollY = window.scrollY
+// window.addEventListener('scroll', () => {
+//     let scrollY = window.scrollY
 
-    for (let i = 0; i < links.length; i++ ) {
-        links[i].classList.remove('header__link-active')
+//     for (let i = 0; i < links.length; i++ ) {
+//         links[i].classList.remove('header__link-active')
         
-        if (scrollY < servicesPos - 100) {
-            links[0].classList.add('header__link-active')
-        }
-        if (scrollY >= servicesPos - 100) {
-            links[1].classList.add('header__link-active')
-        }
-    }
+//         if (scrollY < servicesPos - 100) {
+//             links[0].classList.add('header__link-active')
+//         }
+//         if (scrollY >= servicesPos - 100) {
+//             links[1].classList.add('header__link-active')
+//         }
+//     }
 
-})
+// })
 
-const burger = document.querySelector('.header__burger')
+// const burger = document.querySelector('.header__burger')
 
 
-burger.addEventListener('click', () => {
-    let i = 0;
-    if (i === 0) {
-        i = 1;
-        burger.classList.add('header__burger-active')
-    } else if (i === 1) {
-        i = 0;
-        console.log('close');
-        burger.classList.remove('header__burger-active');
-    }
-})
+// burger.addEventListener('click', () => {
+//     let i = 0;
+//     if (i === 0) {
+//         i = 1;
+//         burger.classList.add('header__burger-active')
+//     } else if (i === 1) {
+//         i = 0;
+//         console.log('close');
+//         burger.classList.remove('header__burger-active');
+//     }
+// })
