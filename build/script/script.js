31d887a90d07c11fb08f78e792ef34e97ecaeb9f
Igor Lugovskoy <86158988+GoshaOlegovich@users.noreply.github.com>
2022-08-16 21:34:05 +0300

    upd

--- build/script/script.js
+++ build/script/script.js
@@ -1,16 +1,35 @@
 
+// Menu
+const menuBtn = document.querySelector('.menu__btn');
+const menuLinks = document.querySelector('.menu__links');
 
-const cases = document.querySelector('.portfolio__cards')
-const projects = document.querySelectorAll('.portfolio__project')
-cases.addEventListener('click', (e) => {
-    let el = +e.target.dataset.num;
-    closeopen(el)
+
+menuBtn.addEventListener('click', () => {   
+    if(menuBtn.classList.contains('menu__btn--active')) {
+        menuBtn.classList.remove('menu__btn--active')
+        menuLinks.classList.remove('menu__links--active')
+    } else {
+        menuBtn.classList.add('menu__btn--active')
+        menuLinks.classList.add('menu__links--active')
+    }
 })
 
+//
 
-const closeopen = (id) => {
-    for (let i = 0; i < projects.length; i ++) {
-        projects[i].style.display = 'none'
-        projects[id].style.display = 'block'
-    }
+const projectLinks = document.querySelectorAll('.project-card')
+const projects = document.querySelectorAll('.project')
+console.log(projects);
+for (let i = 0; i < projectLinks.length; i++) {
+    console.log(projectLinks[i].dataset.index);
+    projectLinks[i].addEventListener('click', (e) => {
+       let el = e.target.dataset.index;
+        projectShow(el)
+    })
 }
+
+const projectShow = (index) => {
+    for (let i = 0; i < projects.length; i++) {
+        projects[i].style.display = "none"
+        projects[index].style.display = "grid"
+    }
+} 
